2b35102b73765cbc4b6e35d60c54ad616e1790b9
sanjivsahani <94617843+sanjivsahani@users.noreply.github.com>
2022-03-11 12:16:49 +0530

    Cors fixrd
    Cors fixed

--- Index.js
+++ Index.js
@@ -6,7 +6,6 @@ const app = express()
 const port = 5000
 app.use(cors())
 app.use(express.json())
-app.use(cors())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
